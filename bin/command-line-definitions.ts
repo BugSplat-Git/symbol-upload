b954180fe0758feb6c9107e6bdd89847c63fdb31
Bobby Galli <bobbyg603@gmail.com>
2021-11-08 10:48:22 -0500

    Updates from code review

--- bin/command-line-definitions.ts
+++ bin/command-line-definitions.ts
@@ -22,34 +22,34 @@ export const argDefinitions: Array<CommandLineDefinition> = [
         alias: 'a',
         type: String,
         typeLabel: '{underline string}',
-        description: 'The name of your application used to post crash reports',
+        description: 'The name of your application. The value of application must match the value used to post crash reports.',
     },
     {
         name: 'version',
         alias: 'v',
         type: String,
         typeLabel: '{underline string}',
-        description: 'The version of your application used to post crash reports',
+        description: 'Your application\'s version. The value of version must match the value used to post crash reports.',
     },
     {
         name: 'user',
         alias: 'u',
         type: String,
         typeLabel: '{underline string} (optional)',
-        description: 'The email address of your BugSplat account. Can also be provided via the SYMBOL_UPLOAD_USER environment variable. If provided --password must also be provided.',
+        description: 'The email address used to log into your BugSplat account. If provided --password must also be provided. This value can also be provided via the SYMBOL_UPLOAD_USER environment variable.',
     },
     {
         name: 'password',
         alias: 'p',
         type: String,
         typeLabel: '{underline string} (optional)',
-        description: 'The password for your BugSplat account. Can also be provided via the SYMBOL_UPLOAD_PASSWORD environment variable. If provided --user must also be provided.',
+        description: 'The password for your BugSplat account. If provided --user must also be provided. This value can also be provided via the SYMBOL_UPLOAD_PASSWORD environment variable.',
     },
     {
         name: 'remove',
         alias: 'r',
         type: Boolean,
-        description: 'Removes symbols for specified database, application, version, and exits.'
+        description: 'Removes symbols for a specified database, application, and version. If this option is provided no other actions are taken.'
     },
     {
         name: 'files',
@@ -57,7 +57,7 @@ export const argDefinitions: Array<CommandLineDefinition> = [
         type: String,
         defaultValue: '*.js.map',
         typeLabel: '{underline string} (optional)',
-        description: 'Glob pattern specifying a file pattern to upload. Defaults to \'*.js.map\'',
+        description: 'Glob pattern that specifies a set of files to upload. Defaults to \'*.js.map\'',
     },
     {
         name: 'directory',
@@ -65,21 +65,21 @@ export const argDefinitions: Array<CommandLineDefinition> = [
         type: String,
         defaultValue: '.',
         typeLabel: '{underline string} (optional)',
-        description: 'Path to base directory to search for symbols and will be combined with the -f glob. Defaults to \'.\'',
+        description: 'Path of the base directory used to search for symbol files. This value will be combined with the --files glob. Defaults to \'.\'',
     }
 ];
 
 export const usageDefinitions: Array<Section> = [
     {
         header: '@bugsplat/symbol-upload',
-        content: 'symbol-upload contains a command line utility and set of libraries to help you upload symbols to BugSplat.',
+        content: 'symbol-upload contains a command line utility and a set of libraries to help you upload symbol files to BugSplat.',
     },
     {
         header: 'Usage',
         optionList: argDefinitions
     },
     {
-        content: 'The -u and -p arguments are optional if you set the environment variables SYMBOL_UPLOAD_USER and SYMBOL_UPLOAD_PASSWORD.'
+        content: 'The -u and -p arguments are not required if you set the environment variables SYMBOL_UPLOAD_USER and SYMBOL_UPLOAD_PASSWORD.'
     },
     {
         header: 'Example',
