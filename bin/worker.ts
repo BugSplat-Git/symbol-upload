06223527ff204ce16b6f2cbcb1a4a8cdb5fe6c19
Bobby Galli <bobbyg603@gmail.com>
2023-10-02 10:37:48 -0400

    fix: retry failed symbol uploads (#64)
    Fixes #62

--- bin/worker.ts
+++ bin/worker.ts
@@ -1,5 +1,6 @@
 import { VersionsApiClient } from '@bugsplat/js-api-client';
 import { createReadStream } from 'fs';
+import retryPromise from 'promise-retry';
 import { SymbolFileInfo } from './symbol-file-info';
 
 export function createWorkersFromSymbolFiles(versionsClient: VersionsApiClient, symbolFiles: SymbolFileInfo[], workerCount: number): Array<UploadWorker> {
@@ -25,6 +26,7 @@ function splitToChunks<T>(array: Array<T>, parts: number): Array<Array<T>> {
 
 export class UploadWorker {
     private createReadStream = createReadStream;
+    private retryPromise = retryPromise;
 
     constructor(
         public readonly id: number,
@@ -32,21 +34,27 @@ export class UploadWorker {
         private versionsClient: VersionsApiClient,
     ) { }
     
-    async upload(database: string, application: string, version: string) {
+    async upload(database: string, application: string, version: string): Promise<void> {
         console.log(`Worker ${this.id} uploading ${this.symbolFileInfos.length} symbol files...`);
 
         for (const symbolFileInfo of this.symbolFileInfos) {
-            console.log(`Worker ${this.id} uploading ${symbolFileInfo.name}...`);
-
-            const file = this.createReadStream(symbolFileInfo.file);
-            const symbolFile = { ...symbolFileInfo, file };
-            await this.versionsClient.postSymbols(database, application, version, [symbolFile])
-                .catch(error => {
-                    symbolFile.file.destroy();
-                    throw error;
-                });
-
-            console.log(`Worker ${this.id} uploaded ${symbolFileInfo.name}!`);
+            await this.retryPromise(
+                (retry) => this.uploadSingle(database, application, version, symbolFileInfo).catch(retry)
+            );
         }
+    }
+
+    private async uploadSingle(database: string, application: string, version: string, symbolFileInfo: SymbolFileInfo): Promise<void> {
+        console.log(`Worker ${this.id} uploading ${symbolFileInfo.name}...`);
+
+        const file = this.createReadStream(symbolFileInfo.file);
+        const symbolFile = { ...symbolFileInfo, file };
+        await this.versionsClient.postSymbols(database, application, version, [symbolFile])
+            .catch(error => {
+                symbolFile.file.destroy();
+                throw error;
+            });
+
+        console.log(`Worker ${this.id} uploaded ${symbolFileInfo.name}!`);
     }
 }
