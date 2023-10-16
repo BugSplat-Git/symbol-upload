ba9994658b2f3c58722a7ef68af0fe6c47b48fd4
Bobby Galli <bobbyg603@gmail.com>
2023-10-16 15:20:41 -0400

    feat: compress on worker thread
    Fixes #54

--- bin/gzip.ts
+++ bin/gzip.ts
@@ -1,11 +1,24 @@
-import { createReadStream, createWriteStream } from "node:fs";
-import { pipeline } from "node:stream/promises";
-import { createGzip } from "node:zlib";
+import { join } from 'node:path';
+import { Worker } from 'node:worker_threads';
+
+const workerScriptPath = join(__dirname, 'gzip-worker.js');
 
 export async function createGzipFile(inputFilePath: string, outputFilePath: string): Promise<void> {
-    await pipeline(
-        createReadStream(inputFilePath),
-        createGzip(),
-        createWriteStream(outputFilePath)
-    );
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(workerScriptPath, {
+            workerData: { inputFilePath, outputFilePath }
+        });
+
+        worker.on('message', (message) => {
+            if (message.type === 'error') {
+                reject(message.error);
+            } else if (message.type === 'done') {
+                resolve();
+            }
+        });
+
+        worker.on('error', (error) => {
+            reject(error);
+        });
+    });
 }
