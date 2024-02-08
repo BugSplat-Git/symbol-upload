29823486e83404672ea17ede55f3501f264580f5
Bobby Galli <bobbyg603@gmail.com>
2024-02-08 10:44:16 -0500

    fix: make pkg compatible

--- src/elf.ts
+++ src/elf.ts
@@ -1,22 +1,34 @@
-import { ElfFile } from 'elfy';
+import { ElfFile } from '@bugsplat/elfy';
 
 export async function tryGetElfUUID(path: string) {
-    using elfFile = await ElfFile.create(path);
-    let { success, section } = await elfFile.tryReadSection('.note.gnu.build-id');
-    
-    if (success) {
-      return getUUID(section!);
-    }
-    
-    ({ success, section } = await elfFile.tryReadSection('.sce_special'));
-  
-    if (success) {
-      return getUUID(section!);
-    }
+  let success: boolean, section: Buffer | undefined;
 
-    return '';
+  // TODO BG use a using statement here when we move away from pkg and can use node 20+
+  let elfFile = await ElfFile.create(path);
+  try {
+    ({ success, section } = await elfFile.tryReadSection('.note.gnu.build-id'));
+  } finally {
+    elfFile.dispose();
+  }
+
+  if (success) {
+    return getUUID(section!);
+  }
+
+  elfFile = await ElfFile.create(path);
+  try {
+    ({ success, section } = await elfFile.tryReadSection('.sce_special'));
+  } finally {
+    elfFile.dispose();
+  }
+
+  if (success) {
+    return getUUID(section!);
+  }
+
+  return '';
 }
 
 function getUUID(section: Buffer) {
-    return section.subarray(0, 20).toString('hex');
+  return section.subarray(0, 20).toString('hex');
 }
