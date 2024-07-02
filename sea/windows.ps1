b0dbb872d5989cc8d1bb4c9f71af4eee09d7adac
Bobby Galli <bobbyg603@gmail.com>
2024-07-02 19:17:51 -0400

    chore: fix windows script

--- sea/windows.ps1
+++ sea/windows.ps1
@@ -1,5 +1,5 @@
 node --experimental-sea-config sea-config.json
-node -e "require('fs').copyFileSync(process.execPath, '.\dist\symbol-upload-win.exe')"
+node -e "require('fs').copyFileSync(process.execPath, '.\\dist\\symbol-upload-win.exe')"
 signtool remove /s .\dist\symbol-upload-win.exe
 npx postject .\dist\symbol-upload-win.exe NODE_SEA_BLOB .\dist\sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
 #signtool sign /fd SHA256 symbol-upload-win.exe 
