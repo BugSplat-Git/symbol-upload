f43d7a1353ac9adcd6edf8ee29bf0613bc4e262e
Bobby Galli <bobbyg603@pm.me>
2024-08-08 13:51:51 -0400

    refactor: simplify glob

--- src/glob.ts
+++ src/glob.ts
@@ -1,16 +1,5 @@
-import { stat } from "node:fs/promises";
-import { fileExists } from "./fs";
 import { glob } from "glob";
 
 export async function globFiles(pattern: string): Promise<string[]> {
-    const result = await glob(pattern);
-    const files = [] as string[];
-
-    for (const file of result) {
-        if (await fileExists(file) && !(await stat(file)).isDirectory()) {
-            files.push(file);
-        }
-    }
-
-    return files;
+    return glob(pattern, { nodir: true });
 }
