e71857512b32d545c1eb9ad0f626bc0d7af7f517
Bobby Galli <bobbyg603@pm.me>
2025-08-14 17:42:03 -0400

    fix: using --localPath option deletes ./tmp folder after copy
    Fixes #168

--- spec/dsym.spec.ts
+++ spec/dsym.spec.ts
@@ -1,24 +1,37 @@
 import { getDSymFileInfos } from '../src/dsym';
+import { safeRemoveTmp } from '../src/tmp';
 
 describe('dsym', () => {
-    describe('getDSymFileInfos', () => {
-        it('should return empty array for non-macho file', async () => {
-            return expectAsync(getDSymFileInfos('spec/support/bugsplat.pdb')).toBeResolvedTo(jasmine.arrayContaining([]));
-        });
-
-        it('should return path, dbgIds, module names for macho files', () => {
-            return expectAsync(getDSymFileInfos('spec/support/bugsplat.app.dSYM')).toBeResolvedTo(jasmine.arrayContaining([
-                {
-                    path: jasmine.stringMatching(/tmp[\/\\]2dd1bd2706fa384da5a3a8265921cf9a[\/\\]BugsplatTester/),
-                    dbgId: '2dd1bd2706fa384da5a3a8265921cf9a',
-                    moduleName: 'BugsplatTester',
-                },
-                {
-                    path: jasmine.stringMatching(/tmp[\/\\]2ce192f6c5963e66b06aa22bde5756a0[\/\\]BugsplatTester/),
-                    dbgId: '2ce192f6c5963e66b06aa22bde5756a0',
-                    moduleName: 'BugsplatTester',
-                }
-            ]));
-        });
+  describe('getDSymFileInfos', () => {
+    it('should return empty array for non-macho file', async () => {
+      return expectAsync(
+        getDSymFileInfos('spec/support/bugsplat.pdb')
+      ).toBeResolvedTo(jasmine.arrayContaining([]));
     });
+
+    it('should return path, dbgIds, module names for macho files', () => {
+      return expectAsync(
+        getDSymFileInfos('spec/support/bugsplat.app.dSYM')
+      ).toBeResolvedTo(
+        jasmine.arrayContaining([
+          {
+            path: jasmine.stringMatching(
+              /tmp(?:-[0-9a-f]*)*[\/\\]2dd1bd2706fa384da5a3a8265921cf9a[\/\\]BugsplatTester/
+            ),
+            dbgId: '2dd1bd2706fa384da5a3a8265921cf9a',
+            moduleName: 'BugsplatTester',
+          },
+          {
+            path: jasmine.stringMatching(
+              /tmp(?:-[0-9a-f]*)*[\/\\]2ce192f6c5963e66b06aa22bde5756a0[\/\\]BugsplatTester/
+            ),
+            dbgId: '2ce192f6c5963e66b06aa22bde5756a0',
+            moduleName: 'BugsplatTester',
+          },
+        ])
+      );
+    });
+
+    afterEach(async () => await safeRemoveTmp());
+  });
 });
