28edc4dfc0c46de62162bcb0e1cee6d5f10c7b2e
Bobby Galli <bobbyg603@pm.me>
2025-08-20 07:09:21 -0400

    fix: using --localPath option deletes ./tmp folder after copy (#169)

--- spec/compression.spec.ts
+++ spec/compression.spec.ts
@@ -6,61 +6,65 @@ import { join } from 'node:path';
 import workerpool from 'workerpool';
 import extract from 'extract-zip';
 import { cwd } from 'node:process';
-const pool = workerpool.pool(join(import.meta.dirname, '../src/compression.mjs'));
+const pool = workerpool.pool(
+  join(import.meta.dirname, '../src/compression.mjs')
+);
 
 describe('gzip', () => {
-    describe('createGzipFile', () => {
-        const tmpFileContents = 'hello world'
-        const tmpFilePath = 'test.txt';
-        const gzipFilePath = 'test.txt.gz';
-        beforeEach(async () => {
-            await writeFile(tmpFilePath, tmpFileContents);
-            await pool.exec('createGzipFile', [tmpFilePath, gzipFilePath]);
-        });
-
-        it('should create a gzip file', async () => {
-            const stream = ReadStream.toWeb(createReadStream(gzipFilePath).pipe(createGunzip()));
-            const result = await streamToString(stream);
-            expect(result).toEqual(tmpFileContents);
-        });
-
-        afterEach(async () => {
-            await rm(gzipFilePath);
-            await rm(tmpFilePath);
-        });
+  describe('createGzipFile', () => {
+    const tmpFileContents = 'hello world';
+    const tmpFilePath = 'test.txt';
+    const gzipFilePath = 'test.txt.gz';
+    beforeEach(async () => {
+      await writeFile(tmpFilePath, tmpFileContents);
+      await pool.exec('createGzipFile', [tmpFilePath, gzipFilePath]);
     });
 
-    describe('createZipFile', () => {
-        const tmpFileContents = 'hello world'
-        const tmpFilePath = 'test.txt';
-        const zipFilePath = 'test.txt.zip';
-        beforeEach(async () => {
-            await writeFile(tmpFilePath, tmpFileContents);
-            await pool.exec('createZipFile', [tmpFilePath, zipFilePath]);
-        });
-
-        it('should create a zip file', async () => {
-            const dir = cwd();
-            await rm(tmpFilePath);
-            await extract(zipFilePath, { dir });
-            const stream = ReadStream.toWeb(createReadStream(tmpFilePath));
-            const result = await streamToString(stream);
-            expect(result).toEqual(tmpFileContents);
-        });
-
-        afterEach(async () => {
-            await rm(zipFilePath);
-            await rm(tmpFilePath);
-        });
+    it('should create a gzip file', async () => {
+      const stream = ReadStream.toWeb(
+        createReadStream(gzipFilePath).pipe(createGunzip())
+      );
+      const result = await streamToString(stream);
+      expect(result).toEqual(tmpFileContents);
     });
+
+    afterEach(async () => {
+      await rm(gzipFilePath);
+      await rm(tmpFilePath);
+    });
+  });
+
+  describe('createZipFile', () => {
+    const tmpFileContents = 'hello world';
+    const tmpFilePath = 'test.txt';
+    const zipFilePath = 'test.txt.zip';
+    beforeEach(async () => {
+      await writeFile(tmpFilePath, tmpFileContents);
+      await pool.exec('createZipFile', [tmpFilePath, zipFilePath]);
+    });
+
+    it('should create a zip file', async () => {
+      const dir = cwd();
+      await rm(tmpFilePath);
+      await extract(zipFilePath, { dir });
+      const stream = ReadStream.toWeb(createReadStream(tmpFilePath));
+      const result = await streamToString(stream);
+      expect(result).toEqual(tmpFileContents);
+    });
+
+    afterEach(async () => {
+      await rm(zipFilePath);
+      await rm(tmpFilePath);
+    });
+  });
 });
 
 async function streamToString(stream: ReadableStream) {
-    const chunks = [] as Uint8Array[];
+  const chunks = [] as Uint8Array[];
 
-    for await (const chunk of stream) {
-        chunks.push(Buffer.from(chunk));
-    }
+  for await (const chunk of stream) {
+    chunks.push(Buffer.from(chunk));
+  }
 
-    return Buffer.concat(chunks).toString("utf-8");
+  return Buffer.concat(chunks).toString('utf-8');
 }
--- src/tmp.ts
+++ src/tmp.ts
@@ -1,23 +1,24 @@
+import { randomUUID } from 'crypto';
 import { rm } from 'fs/promises';
 import { join } from 'path';
 import promiseRetry from 'promise-retry';
 
 const currentDirectory = process ? process.cwd() : __dirname;
-
-export const tmpDir = join(currentDirectory, 'tmp');
+const uuid = randomUUID();
+export const tmpDir = join(currentDirectory, `tmp-${uuid}`);
 
 export async function safeRemoveTmp(remover = rm): Promise<void> {
-    try {
-        await promiseRetry(
-            (retry) => remover(tmpDir, { recursive: true, force: true }).catch(retry),
-            {
-                minTimeout: 0, // First retry immediately
-                maxTimeout: 2000, // Subsequent retries delay by 2 seconds
-                factor: 2, // Exponential backoff
-                retries: 4, // Try 4 times
-            }
-        );
-    } catch (error) {
-        console.error(`Could not delete ${tmpDir}!`, error);
-    }
+  try {
+    await promiseRetry(
+      (retry) => remover(tmpDir, { recursive: true, force: true }).catch(retry),
+      {
+        minTimeout: 0, // First retry immediately
+        maxTimeout: 2000, // Subsequent retries delay by 2 seconds
+        factor: 2, // Exponential backoff
+        retries: 4, // Try 4 times
+      }
+    );
+  } catch (error) {
+    console.error(`Could not delete ${tmpDir}!`, error);
+  }
 }
